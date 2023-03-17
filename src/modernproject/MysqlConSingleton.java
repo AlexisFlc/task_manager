b9a4e587fb1b16cf7fecbf574b0c21c7c75e51b3
mj396287 <jarreaumik@hotmail.com>
2023-03-17 15:08:13 +0100

    userAction is service

--- src/modernproject/MysqlConSingleton.java
+++ src/modernproject/MysqlConSingleton.java
@@ -38,28 +38,5 @@ public class MysqlConSingleton {
     }
 
 
-    public UserInfo getUserInfo(String username) throws SQLException {
-        PreparedStatement stmt = con.prepareStatement("select * from users where username = ?");
-        stmt.setString(1, username);
-        ResultSet rs = stmt.executeQuery();
-        if (rs.next()) {
-            String name = rs.getString("last_name");
-            String firstName = rs.getString("first_name");
-            String password = rs.getString("password");
-            String usernamee = rs.getString("username");
-            UserInfo user = new UserInfo(name, firstName, usernamee, password);
-            return user;
-        }
-        return null;
-    }
 
-    public void addUser(UserInfo user) throws SQLException {
-        PreparedStatement stmt = con.prepareStatement("insert into users (last_name, first_name, password, username) values (?, ?, ?, ?)");
-        System.out.println(user.getUserName()+ user.getFirstName()+user.getLastName()+ user.getPassword());
-        stmt.setString(1, user.getFirstName());
-        stmt.setString(2, user.getLastName());
-        stmt.setString(3, user.getPassword());
-        stmt.setString(4, user.getUserName());
-        stmt.executeUpdate();
-    }
 }
--- src/modernproject/UserAction.java
+++ src/modernproject/UserAction.java
@@ -7,22 +7,49 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class UserAction {
-	private static ArrayList <UserInfo> userList = new ArrayList<UserInfo>();
-	private static MysqlConSingleton  mysqlCon = MysqlConSingleton.getInstance();
+	private static final MysqlConSingleton  mysqlCon = MysqlConSingleton.getInstance();
 
 	//takes the incoming UserInfo object and serializes the information into a text file
 	public static void serializeSignUp(UserInfo user) throws IOException, ClassNotFoundException, SQLException {
-		mysqlCon.addUser(user);
+		addUser(user);
 	}
 
 	//the incoming String will be the username of the user and it will read the database for the information
 	public static UserInfo readUserData(String username) throws IOException, ClassNotFoundException, SQLException {
-		UserInfo user = mysqlCon.getUserInfo(username);
+		UserInfo user = getUserInfo(username);
 		return user;
+	}
+
+
+	public static UserInfo getUserInfo(String username) throws SQLException {
+		PreparedStatement stmt = mysqlCon.getCon().prepareStatement("select * from users where username = ?");
+		stmt.setString(1, username);
+		ResultSet rs = stmt.executeQuery();
+		if (rs.next()) {
+			String name = rs.getString("last_name");
+			String firstName = rs.getString("first_name");
+			String password = rs.getString("password");
+			String usernamee = rs.getString("username");
+			UserInfo user = new UserInfo(name, firstName, usernamee, password);
+			return user;
+		}
+		return null;
+	}
+
+	public static void addUser(UserInfo user) throws SQLException {
+		PreparedStatement stmt = mysqlCon.getCon().prepareStatement("insert into users (last_name, first_name, password, username) values (?, ?, ?, ?)");
+		System.out.println(user.getUserName()+ user.getFirstName()+user.getLastName()+ user.getPassword());
+		stmt.setString(1, user.getFirstName());
+		stmt.setString(2, user.getLastName());
+		stmt.setString(3, user.getPassword());
+		stmt.setString(4, user.getUserName());
+		stmt.executeUpdate();
 	}
 
 }
